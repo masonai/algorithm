a164fe00b87d3372533913e998aca05ba066d0f6
masonai <masonai>
2020-07-31 11:54:24 +0900

    complexity problem again

--- codility/Lesson4-2.MaxCounters.py
+++ codility/Lesson4-2.MaxCounters.py
@@ -2,22 +2,23 @@
 def solution(N, A):
     arr_ = [0] * N
 
-    for i in A:
-        if i == N+1:
-            max_c = max(arr_)
-            for j in range(N):
-                arr_[j] = max_c
+    for i in range(len(A)):
+        idx = A[i] - 1
+
+        if idx == N:
+            current_max = max(arr_)
+            arr_ = [current_max] * N
             continue
 
-        arr_[i-1] += 1
+        arr_[idx] += 1
 
     return arr_
     pass
 
 
 N = 5
-A = [3,4,4,6,1,4,4]
-#A = [1,1,2,4]
+#A = [3,4,4,6,1,4,4]
+A = [1,1,2,6,6]
 
 a = solution(N, A)
 print(a)
