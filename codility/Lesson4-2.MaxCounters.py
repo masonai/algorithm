1900d3a8e262b66480dce3b55e8e332a49f43b98
masonai <masonai>
2020-08-07 12:26:18 +0900

    score 88%. Detected time complexity: O(N + M)

--- codility/Lesson4-2.MaxCounters.py
+++ codility/Lesson4-2.MaxCounters.py
@@ -2,15 +2,17 @@
 def solution(N, A):
     arr_ = [0] * N
 
+    max_c = 0
     for i in range(len(A)):
         idx = A[i] - 1
 
         if idx == N:
-            current_max = max(arr_)
-            arr_ = [current_max] * N
+#            current_max = max(arr_)
+            arr_ = [max_c] * N
             continue
 
         arr_[idx] += 1
+        max_c = max(arr_[idx], max_c)
 
     return arr_
     pass
@@ -18,7 +20,8 @@ def solution(N, A):
 
 N = 5
 #A = [3,4,4,6,1,4,4]
-A = [1,1,2,6,6]
+#A = [1,1,2,6,6]
+A = [1,2,3,4,1,6]
 
 a = solution(N, A)
 print(a)
